c8c0fb2c217c1794d1720d33cd5486cfe8468c87
Ahmed-360-jr <ahmednassor124@yahoo.com>
2026-04-12 19:38:55 +0300

    Add files via upload

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const API_BASE =
     window.location.hostname === 'localhost'
         ? 'http://localhost:5000'
-        : 'https://your-backend-url.com'; // 🔴 change later
+        : 'https://coachcsca-5.onrender.com'; 
 
 export default API_BASE;
